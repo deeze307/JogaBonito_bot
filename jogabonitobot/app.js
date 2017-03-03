3e5b22fe37204304a13d150bc116305938179684
deeze307 <deeze.designs@gmail.com>
2017-03-03 13:55:58 -0300

    respuesta de bot a chat grupal

--- jogabonitobot/app.js
+++ jogabonitobot/app.js
@@ -5,57 +5,62 @@ const bot = new TeleBot('333576962:AAEg6XnIhsO_3ZTc6KFipZLnzhpp2FmFcjQ');
 
 bot.on('/plantel', msg => {
 	let fromId = msg.from.id;
-
+	let chatId = msg.chat.id;
 	let rta = jogaBonito.formatearLista(fromId);
-	return bot.sendMessage(fromId, rta);
+	return bot.sendMessage(chatId, rta);
 });
 
 bot.on('/voy', msg => {
 	let fromId = msg.from.id;
+	let chatId = msg.chat.id;
 	let user = msg.from.first_name;
 
 	console.log(msg.from);
 
 	let rta = jogaBonito.addUser(user,fromId);  
-	return bot.sendMessage(fromId, rta);
+	return bot.sendMessage(chatId, rta);
 });
 
 bot.on('/novoy', msg => {
 	let fromId = msg.from.id;
+	let chatId = msg.chat.id;
 	let user = msg.from.first_name;
 
 	let rta = jogaBonito.delUser(user,fromId);
-	return bot.sendMessage(fromId, rta);
+	return bot.sendMessage(chatId, rta);
 });
 
 bot.on('/invitar', msg => {
 	let fromId = msg.from.id;
+	let chatId = msg.chat.id;
 	let user = msg.from.first_name;
 	let invitados = msg.text.split(' ');
 	invitados.splice(0, 1);
   
 	_.each(invitados, function(invitado,index){
 		let rta = jogaBonito.addUser(invitado,fromId,user);  
-		return bot.sendMessage(fromId, rta);
+		return bot.sendMessage(chatId, rta);
 	});
 });
 
 bot.on('/nova', msg => {
 	let fromId = msg.from.id;
+	let chatId = msg.chat.id;
 	let user = msg.from.first_name;
 	let invitados = msg.text.split(' ');
 	invitados.splice(0, 1);
   
 	_.each(invitados, function(invitado,index){
 		let rta = jogaBonito.delUser(invitado,fromId,user);  
-		return bot.sendMessage(fromId, rta);
+		return bot.sendMessage(chatId, rta);
 	});
 });
 
 bot.on('/reset',msg => {
 	let fromId = msg.from.id;
+	let chatId = msg.chat.id;
 	let rta = jogaBonito.reiniciarSemana();
-	return bot.sendMessage(fromId,rta);
+	return bot.sendMessage(chatId,rta);
 });
 
 console.log("Wait...");
